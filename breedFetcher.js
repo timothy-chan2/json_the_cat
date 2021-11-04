180e389ada273e13336d1cd560b906baa0dc0ba1
Timothy Cheu-Chun Chan <timothy.chan2@mail.mcgill.ca>
2021-11-04 18:52:10 +0000

    Move command line arg & request npm package callback function into index.js & export the function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,7 @@
 const request = require('request');
-let breed = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error) {
-    console.log('Error:', error);
-  } else if (data[0] === undefined) {
-    console.log('Breed name is not found');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breed, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, callback);
+};
+
+module.exports = { fetchBreedDescription };
